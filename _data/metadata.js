ffe4fd57aa87225699f342743ac1a87c7f376386
Nikita <g.zyzyx@gmail.com>
2025-06-29 13:32:41 +0300

    Очистка home.njk и пересборка проекта

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,5 +1,6 @@
-module.exports = {
+export default {
   title: "Музей жетонов метро",
   description: "Каталог коллекции жетонов, билетов и объектов городского транспорта",
-  url: "https://zander-museum.netlify.app" // укажи свой домен, если он будет отличаться
+  url: "https://zander-museum.netlify.app", // укажи свой домен, если он будет отличаться
+  author: "Zander Nikita",
 };
--- eleventy.config.js
+++ eleventy.config.js
@@ -153,7 +153,8 @@ export const config = {
 	// Optional items:
 	// -----------------------------------------------------------------
 
-	// If your site deploys to a subdirectory, change `pathPrefix`.
+	// If your site deploys to a sub
+ectory, change `pathPrefix`.
 	// Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
 
 	// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
